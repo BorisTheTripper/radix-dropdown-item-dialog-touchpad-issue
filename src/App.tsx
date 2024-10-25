840d1d10bd990beb82e1fe007d5f4e66f46a3768
Boris Adaev <mr.bobod@mail.ru>
2024-10-25 23:20:04 +0300

    working: simple menu item

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,48 @@
 import { Button } from './components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+} from './components/ui/dialog';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from './components/ui/dropdown-menu';
 
 function App() {
   return (
     <div className="h-full grid place-items-center">
-      <Button children="hello" />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button>open dropdown</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem>...</DropdownMenuItem>
+          <DropdownMenuItem>...</DropdownMenuItem>
+          <Dialog>
+            <DialogTrigger asChild>
+              <DropdownMenuItem onSelect={(e) => e.preventDefault()}>
+                open dialog
+              </DropdownMenuItem>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogTitle>dialog title</DialogTitle>
+              <DialogDescription>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+                cupiditate, aut expedita dolores sunt doloribus, id facere odit
+                a dignissimos non qui autem, delectus repellat odio voluptatum
+                eos quas incidunt placeat dolorum minima officia. Tenetur
+                deserunt accusantium sunt nostrum accusamus? Totam velit minima
+                laboriosam consequatur quam nisi! Veniam, sunt sint!
+              </DialogDescription>
+            </DialogContent>
+          </Dialog>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 }
