64c2cce47f6fe665580c1e33f23a1ca7e3c0064e
Boris Adaev <mr.bobod@mail.ru>
2024-10-26 13:33:35 +0300

    problem: dialog reopning on touchpad click

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { Button } from './components/ui/button';
 import {
   Dialog,
@@ -11,69 +10,33 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from './components/ui/dropdown-menu';
 
 function App() {
-  const [reportReason, setReportReason] = useState<string | null>(null);
-
   return (
-    <div className="h-full grid place-items-center">
+    <div className="h-[20%] grid place-items-center">
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button>open dropdown</Button>
         </DropdownMenuTrigger>
+
         <DropdownMenuContent>
-          <DropdownMenuItem>...</DropdownMenuItem>
-          <DropdownMenuItem>...</DropdownMenuItem>
-
           <Dialog>
-            <DropdownMenuSub>
-              <DropdownMenuSubTrigger>report post</DropdownMenuSubTrigger>
-
-              <DropdownMenuSubContent>
-                <DialogTrigger asChild>
-                  <DropdownMenuItem
-                    onSelect={(e) => {
-                      e.preventDefault();
-                      setReportReason('spam');
-                    }}
-                  >
-                    Spam
-                  </DropdownMenuItem>
-                </DialogTrigger>
-                <DialogTrigger asChild>
-                  <DropdownMenuItem
-                    onSelect={(e) => {
-                      e.preventDefault();
-                      setReportReason('toxicity');
-                    }}
-                  >
-                    Toxicity
-                  </DropdownMenuItem>
-                </DialogTrigger>
-                <DialogTrigger asChild>
-                  <DropdownMenuItem
-                    onSelect={(e) => {
-                      e.preventDefault();
-                      setReportReason(null);
-                    }}
-                  >
-                    Other
-                  </DropdownMenuItem>
-                </DialogTrigger>
-
-                <DialogContent>
-                  <DialogTitle>Report post</DialogTitle>
-                  <DialogDescription>
-                    Cited reason: <strong>{reportReason}</strong>
-                  </DialogDescription>
-                </DialogContent>
-              </DropdownMenuSubContent>
-            </DropdownMenuSub>
+            <DialogTrigger asChild>
+              <DropdownMenuItem onSelect={(e) => e.preventDefault()}>
+                open dialog
+              </DropdownMenuItem>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogTitle>Dialog</DialogTitle>
+              <DialogDescription>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Perspiciatis iste illum in quaerat delectus, repellendus neque
+                non possimus, consectetur dicta saepe corrupti provident culpa.
+                Incidunt rerum esse pariatur officia labore.
+              </DialogDescription>
+            </DialogContent>
           </Dialog>
         </DropdownMenuContent>
       </DropdownMenu>
